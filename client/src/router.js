89abb1bc1829fade0e7b8aecc77b7d4f67448d4a
Marten T. Compoc <martenc@gmail.com>
2019-02-28 12:08:05 -0800

    add routing and page transitions

--- client/src/router.js
+++ client/src/router.js
@@ -2,6 +2,13 @@ import Vue from "vue";
 import Router from "vue-router";
 import Home from "./components/Home.vue";
 
+import AddPost from './components/Posts/AddPost.vue'
+import Posts from './components/Posts/Posts.vue'
+
+import Profile from './components/Auth/Profile.vue'
+import Signin from './components/Auth/Signin.vue'
+import Signup from './components/Auth/Signup.vue'
+
 Vue.use(Router);
 
 export default new Router({
@@ -12,6 +19,31 @@ export default new Router({
       path: "/",
       name: "home",
       component: Home
+    },
+    {
+      path: "/posts",
+      name: "Posts",
+      component: Posts
+    },
+    {
+      path: "/post/add",
+      name: "AddPost",
+      component: AddPost
+    },
+    {
+      path: "/profile",
+      name: "Profile",
+      component: Profile
+    },
+    {
+      path: "/signin",
+      name: "Signin",
+      component: Signin
+    },
+    {
+      path: "/Signup",
+      name: "Signup",
+      component: Signup
     }
   ]
 });
