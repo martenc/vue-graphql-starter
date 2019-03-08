e2308901ed8e2de6d18282c5fe7970e958f2716a
Marten T. Compoc <martenc@gmail.com>
2019-03-08 09:35:50 -0800

    add carousel

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,12 +1,12 @@
 <template>
-  <v-container>
-    <h1>Home</h1>
-    <ul v-for="post in getPosts" :key="post._id">
-      <li>
-        {{post.title}} {{post.imageUrl}} {{post.description}}
-      </li>
-      <li>{{post.likes}}</li>
-    </ul>
+  <v-container text-xs-center v-if="getPosts">
+    <v-flex xs12>
+      <v-carousel v-bind="{ 'cycle': true }" interval="3000">
+        <v-carousel-item v-for="post in getPosts" :key="post._id" :src="post.imageUrl">
+          <h1 id="carousel__title">{{post.title}}</h1>
+        </v-carousel-item>
+      </v-carousel>
+    </v-flex>
   </v-container>
 </template>
 
@@ -32,3 +32,17 @@ export default {
   }
 };
 </script>
+
+<style>
+#carousel__title {
+  position: absolute;
+  background-color: rgba(0, 0, 0, 0.5);
+  color: white;
+  border-radius: 5px 5px 0 0;
+  padding: 0.5em;
+  margin: 0 auto;
+  bottom: 50px;
+  left: 0;
+  right: 0;
+}
+</style>
